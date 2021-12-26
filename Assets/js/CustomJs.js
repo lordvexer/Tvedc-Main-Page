ecdbf712fe16e6173d3b1bd88ab07bf77f9e2332
HamidReza Mahmoudzadeh <h.nypd@yahoo.com>
2021-12-26 13:03:23 +0330

    tabs added

--- Assets/js/CustomJs.js
+++ Assets/js/CustomJs.js
@@ -1,6 +1,6 @@
 
 
-/* Fix NavBar */
+//Fix NavBar
 
 window.onscroll = function() {myFunction()};
 
@@ -12,5 +12,31 @@ function myFunction() {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
+  }
+}
+
+// Tabs
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+//Navbar
+
+function NavFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
 }
